6fe3deb96c1dbca451b9f0b6869bfc09fa9b0813
Jlin72 <jhonnylin72@gmail.com>
2022-10-03 17:23:28 -0400

    Dev server

--- webpack.config.js
+++ webpack.config.js
@@ -8,11 +8,12 @@ module.exports = {
         path: path.resolve(__dirname, "src/dist")
     },
     devServer: {
-        static: {
-            directory: path.join(__dirname, 'src'),
-        },
-        compress: true,
-        port: 8080
+        port:8080,
+        open:true,
+        compress:true,
+        hot:true,
+        liveReload:true,
+        static: path.join(__dirname, 'src')
     },
     module: {
         rules: [
